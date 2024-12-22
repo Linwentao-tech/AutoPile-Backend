99b8e23b20d8764bd2e2c6caa99a1420e8ac87ff
Max Lin <linwentao888@gmail.com>
2024-12-23 03:07:46 +1100

    DTO implementation

---
+++ AutoPile.DOMAIN/DTOs/Responses/OrderItemResponseDTO.cs
@@ -0,0 +1,20 @@
+﻿using AutoPile.DOMAIN.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoPile.DOMAIN.DTOs.Responses
+{
+    public class OrderItemResponseDTO
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal ProductPrice { get; set; }
+        public int Quantity { get; set; }
+        public decimal TotalPrice { get; set; }
+        public ProductResponseDTO Product { get; set; }
+    }
+}
