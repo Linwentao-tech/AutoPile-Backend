b96d23435b710e256624787048e63236c6ad9403
Max Lin <linwentao888@gmail.com>
2024-12-25 22:00:44 +1100

    update customexception

---
+++ AutoPile.DATA/Exceptions/BadRequestException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoPile.DATA.Exceptions
+{
+    public class BadRequestException : AbstractHTTPexception
+    {
+        public BadRequestException() : base(400, "Bad Request")
+        {
+        }
+
+        public BadRequestException(string message) : base(400, message)
+        {
+        }
+    }
+}
