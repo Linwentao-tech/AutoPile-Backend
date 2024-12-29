5932cf41a55d31542751dabb2c4f634ad0ebf2e9
Max Lin <linwentao888@gmail.com>
2024-12-30 01:56:34 +1100

    implement reset password token check

---
+++ AutoPile.DOMAIN/DTOs/Requests/ValidateTokenRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoPile.DOMAIN.DTOs.Requests
+{
+    public class ValidateTokenRequest
+    {
+        public string Email { get; set; }
+        public string Token { get; set; }
+    }
+}
